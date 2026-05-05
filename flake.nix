d9fc2405406f8dbd68376735855a80cf98576007
Philipp David <pd@3b.pm>
2026-05-05 10:19:27 +0200

    use only nixpkgs as input

--- flake.nix
+++ flake.nix
@@ -2,21 +2,13 @@
   description = "Port of ttf-ms-win11{,-fod}-auto* packages to Nix";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
-  inputs.devshell.url = "github:numtide/devshell";
-  inputs.flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
 
-  outputs = inputs @ {
-    nixpkgs,
-    flake-parts,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;}
+  outputs =
     {
-      imports = [
-        inputs.devshell.flakeModule
-        inputs.flake-parts.flakeModules.easyOverlay
-      ];
+      self,
+      nixpkgs,
+    }:
+    let
       systems = [
         "i686-linux"
         "x86_64-linux"
@@ -24,66 +16,86 @@
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-      perSystem = {
-        pkgs,
-        system,
-        config,
-        ...
-      }: {
-        _module.args.pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-        packages = let
-          allPackages =
-            pkgs.lib.mapAttrs' (jsonFileName: _: let
-              pkgName = pkgs.lib.removeSuffix ".json" jsonFileName;
-              pkgData = builtins.fromJSON (builtins.readFile ./pkgs/${jsonFileName});
-            in {
-              name = pkgName;
-              value = (import ./mkPackage.nix) {
-                inherit pkgs pkgName;
-                inherit (pkgData) archive files iso outputHash parentDir version;
-              };
-            })
-            (builtins.readDir ./pkgs);
+      forAllSystems = f: nixpkgs.lib.genAttrs systems f;
+      pkgsFor = system: nixpkgs.legacyPackages.${system};
+      packagesFor =
+        system:
+        let
+          pkgs = pkgsFor system;
         in
-          allPackages
-          // rec {
-            ttf-ms-win11-auto-all = pkgs.symlinkJoin {
-              name = "ttf-ms-win11-auto-all";
-              paths = pkgs.lib.attrValues allPackages;
-            };
-            default = ttf-ms-win11-auto-all;
-          };
-        overlayAttrs = builtins.listToAttrs (
-          map (pkgName: {
+        pkgs.lib.mapAttrs' (
+          jsonFileName: _:
+          let
+            pkgName = pkgs.lib.removeSuffix ".json" jsonFileName;
+            pkgData = builtins.fromJSON (builtins.readFile ./pkgs/${jsonFileName});
+          in
+          {
             name = pkgName;
-            value = config.packages.${pkgName};
-          }) (pkgs.lib.attrNames config.packages)
-        );
-        devshells.default = {
-          commands = [
-            {
-              help = "Update the package definitions in pkgs.json";
-              name = "update-pkgs";
-              command = "${pkgs.bash}/bin/bash ${./.}/update-pkgs.sh update";
-            }
-            {
-              help = "Force update the package definitions in pkgs.json";
-              name = "update-pkgs-force";
-              command = "${pkgs.bash}/bin/bash ${./.}/update-pkgs.sh force-update";
-            }
-          ];
-          packages = with pkgs; [
+            value = (import ./mkPackage.nix) {
+              inherit pkgs pkgName;
+              inherit (pkgData)
+                archive
+                files
+                iso
+                outputHash
+                parentDir
+                version
+                ;
+            };
+          }
+        ) (builtins.readDir ./pkgs);
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+          allPackages = packagesFor system;
+        in
+        allPackages
+        // rec {
+          ttf-ms-win11-auto-all = pkgs.symlinkJoin {
+            name = "ttf-ms-win11-auto-all";
+            paths = pkgs.lib.attrValues allPackages;
+          };
+          default = ttf-ms-win11-auto-all;
+        }
+      );
+      overlays.default = final: _prev: packagesFor final.stdenv.hostPlatform.system final;
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+          updateDeps = with pkgs; [
+            bash
             git
             jq
             parallel
-            (pkgs.python3.withPackages (p: [
-              p.fontforge
-            ]))
+            (python3.withPackages (p: [ p.fontforge ]))
           ];
-        };
-      };
+          update-pkgs = pkgs.writeShellApplication {
+            name = "update-pkgs";
+            runtimeInputs = updateDeps;
+            text = ''
+              bash ${self}/update-pkgs.sh update
+            '';
+          };
+          update-pkgs-force = pkgs.writeShellApplication {
+            name = "update-pkgs-force";
+            runtimeInputs = updateDeps;
+            text = ''
+              bash ${self}/update-pkgs.sh force-update
+            '';
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = updateDeps ++ [
+              update-pkgs
+              update-pkgs-force
+            ];
+          };
+        }
+      );
     };
 }
